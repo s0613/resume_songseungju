6a793690b69c0879856d592e5b2d6ad4cebff050
SongSeungJu <wkwjsrj5079@gmail.com>
2025-02-14 17:06:54 +0900

    appspec after update

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,1 +1,6 @@
-echo "> after-install.sh 실행"
+#!/bin/bash
+set -e
+
+echo "> after-install.sh 실행" | tee -a /home/ec2-user/deploy.log
+echo "현재 디렉토리: $(pwd)" | tee -a /home/ec2-user/deploy.log
+ls -al /home/ec2-user/resume_songseungju | tee -a /home/ec2-user/deploy.log
